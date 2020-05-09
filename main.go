3d8d54091311a127c04a680782c853e9f2b17de1
Santiago Mino <santiago.mino@mercadolibre.com>
2020-05-09 00:01:10 -0300

    bd connection & router handler

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/santiagomb/udemy-twittor/bd"
+	"github.com/santiagomb/udemy-twittor/handlers"
+	"log"
+)
+
 func main() {
-	
+	success := bd.Ping()
+	if success {
+		log.Println("Starting server ...")
+		handlers.Handlers()
+	}
 }
